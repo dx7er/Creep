6fdc708dc53c0050805e6b2c82d7f4d06d10ae01
Saqlain Naqvi <79792270+naqviO7@users.noreply.github.com>
2021-12-10 18:54:28 +0500

    Update reqs.sh

--- reqs.sh
+++ reqs.sh
@@ -3,20 +3,8 @@
 green="\e[32m"
 yellow="\e[33m"
 
-clear
-
-echo -e $yellow
-echo "[!] Checking Requirements."
-
-sleep 2
-
-chmod +x fattynigger.py
-
-chmod +x taxes.py
-
-chmod +x wifiter.sh
-
-sleep 2
-
-echo -e $green
+clear && echo -e $yellow
+echo "[!] Checking Requirements." && sleep 2
+chmod +x fattynigger.py taxes.py wifiter.sh
+sleep 2 && echo -e $green
 echo "[!] Requirements Good to GO."
