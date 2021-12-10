389da1840d7af6b61420a02d39b7d308457b7988
Saqlain Naqvi <79792270+naqviO7@users.noreply.github.com>
2021-12-10 16:35:27 +0500

    Create reqs.sh

---
+++ reqs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+green="\e[32m"
+yellow="\e[33m"
+
+clear
+
+echo -e $yellow
+echo "[!] Checking Requirements."
+
+sleep 2
+
+chmod +x fattynigger.py
+
+chmod +x taxes.py
+
+chmod +x wifiter.sh
+
+sleep 2
+
+echo -e $green
+echo "[!] Requirements Good to GO."
